81433b1e5ee147c9d90de41564f904d2327d3873
Glitch (destroytheattacker) <none>
2020-06-10 03:13:06 +0000

    🏖🎉 Checkpoint ./lib/definitions.js:13763590/10320 ./server.js:13763590/162

--- server.js
+++ server.js
@@ -4634,7 +4634,7 @@ var maintainloop = (() => {
                         choice = [[Class.elite_gunner], 6, 'a', 'nest'];
                         break;
                      case 2: 
-                        choice = [[Class.elite_gunner], 6, 'a', 'nest'];
+                        choice = [[Class.closerarena], 6, 'a', 'nest'];
                         break;
                      case 3: 
                         choice = [[Class.elite_gunner], 12, 'a', 'nest'];
@@ -4661,7 +4661,7 @@ var maintainloop = (() => {
                         choice = [[Class.elite_gunner], 20, 'a', 'nest'];
                         break;
                     case 11: 
-                        choice = [[Class.closerarena], 15, 'a', 'nest'];
+                        choice = [[Class.elite_gunner], 15, 'a', 'nest'];
                    
                 
                 }
