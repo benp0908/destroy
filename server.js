142956a5ede7dc1f1d08a237e54e317414c6aa31
Glitch (destroytheattacker) <none>
2020-06-15 16:46:53 +0000

    🍡🛎 Checkpoint ./server.js:13763590/2

--- server.js
+++ server.js
@@ -4663,7 +4663,7 @@ var maintainloop = (() => {
                      case 11: 
                         choice = [[Class.closerarena], 10, 'a', 'nest'];
                         break;
-                     case 12: 
+                     case 13: 
                         choice = [[Class.closerarena], 12, 'a', 'nest'];
        
                    
