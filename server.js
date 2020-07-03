ba7e5e732dd3ae64e45826baeb0ce131ef00cb77
Glitch (destroytheattacker) <none>
2020-07-03 21:18:20 +0000

    👠🐞 Checkpoint ./server.js:13763590/145 ./lib/definitions.js:13763590/10665

--- server.js
+++ server.js
@@ -4637,8 +4637,8 @@ var maintainloop = (() => {
 
                       break;
                      case 2: 
-                        choice = [[Class.elite_gunner], 3, 'castle', 'nest'];
-                                             sockets.broadcast('3 Attackers have Spawned!');
+                        choice = [[Class.elite_sprayer], 1, 'castle', 'nest'];
+                                             sockets.broadcast('The Summoner has Spawned!');
 
                         break;
                      case 3: 
@@ -4652,32 +4652,32 @@ var maintainloop = (() => {
 
                     break;
                      case 5: 
-                        choice = [[Class.elite_gunner], 12, 'castle', 'nest'];
-                                                                 sockets.broadcast('12 Attackers have Spawned!');
+                        choice = [[Class.elite_gunner], 8, 'castle', 'nest'];
+                                                                 sockets.broadcast('8 Attackers have Spawned!');
 
                         break;
                      case 6: 
-                        choice = [[Class.elite_gunner], 12, 'castle', 'nest'];
-                                                                                     sockets.broadcast('12 Attackers have Spawned!');
+                        choice = [[Class.elite_gunner], 8, 'castle', 'nest'];
+                                                                                     sockets.broadcast('8 Attackers have Spawned!');
 
                         break;
                      case 7: 
-                        choice = [[Class.elite_gunner], 10, 'castle', 'nest'];
-                        break;                                                                 sockets.broadcast('12 Attackers have Spawned!');
+                        choice = [[Class.elite_gunner], 8, 'castle', 'nest'];
+                        break;                                                                 sockets.broadcast('8 Attackers have Spawned!');
 
                      case 8: 
                         choice = [[Class.elite_gunner], 10, 'castle', 'nest'];
-                                                                                     sockets.broadcast('12 Attackers have Spawned!');
+                                                                                     sockets.broadcast('10 Attackers have Spawned!');
 
                         break;
                      case 9: 
                         choice = [[Class.elite_gunner], 10, 'castle', 'nest'];
-                                                                                     sockets.broadcast('12 Attackers have Spawned!');
+                                                                                     sockets.broadcast('10 Attackers have Spawned!');
 
                         break;
                      case 10: 
                         choice = [[Class.elite_gunner], 10, 'castle', 'nest'];
-                                                                                     sockets.broadcast('12 Attackers have Spawned!');
+                                                                                     sockets.broadcast('10 Attackers have Spawned!');
 
                         break;
                      case 11: 
