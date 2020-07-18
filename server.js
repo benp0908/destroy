cd895c077ac475a97d61fa1f60fc6fa61bc57a8d
Glitch (destroytheattacker) <none>
2020-07-18 01:04:45 +0000

    Rewound to commit a72ebe0c4c91fd11e5464d6234f6027959cb84e9 + 0 edits

--- server.js
+++ server.js
@@ -4628,42 +4628,60 @@ var maintainloop = (() => {
                 switch (wave) {
                     case 0: 
                         choice = [[Class.elite_gunner], 3, 'castle', 'nest'];
+
                         break;
                     case 1: 
                         choice = [[Class.elite_gunner], 3, 'castle', 'nest'];
+
                       break;
                      case 2: 
                         choice = [[Class.elite_gunner], 3, 'castle', 'nest'];
+
                         break;
                      case 3: 
                         choice = [[Class.elite_gunner], 6, 'castle', 'nest'];
+
                     break;
                      case 4: 
                         choice = [[Class.elite_gunner], 6, 'castle', 'nest'];
+
                     break;
                      case 5: 
                         choice = [[Class.elite_gunner], 12, 'castle', 'nest'];
+
                         break;
                      case 6: 
                         choice = [[Class.elite_gunner], 12, 'castle', 'nest'];
+
                         break;
                      case 7: 
-                        choice = [[Class.elite_gunner], 12, 'castle', 'nest'];
-                        break;
+                        choice = [[Class.elite_gunner], 10, 'castle', 'nest'];
+
                      case 8: 
-                        choice = [[Class.elite_gunner], 12, 'castle', 'nest'];
+                        choice = [[Class.elite_gunner], 10, 'castle', 'nest'];
+
                         break;
                      case 9: 
-                        choice = [[Class.elite_gunner], 12, 'castle', 'nest'];
+                        choice = [[Class.elite_gunner], 10, 'castle', 'nest'];
+
                         break;
                      case 10: 
-                        choice = [[Class.elite_gunner], 12, 'castle', 'nest'];
+                        choice = [[Class.elite_gunner], 15, 'castle', 'nest'];
+                                            sockets.broadcast('Final Wave: All out Attack!');
+
                         break;
                      case 11: 
-                        choice = [[Class.closerarena], 10, 'a', 'nest'];
+                        choice = [[Class.closerarena], 10, 'a', 'norm'];
+                                            sockets.broadcast('BLUE DEFENDERS HAVE WON THE GAME!');
+
+                        sockets.broadcast('Arena Closed: No players can join!');
+
+
                         break;
                      case 12: 
-                        choice = [[Class.closerarena], 12, 'a', 'nest'];
+                        choice = [[Class.closerarena], 12, 'a', 'norm'];
+                                            sockets.broadcast('Closing!');
+
        
                    
                 
@@ -4732,14 +4750,14 @@ var maintainloop = (() => {
             //spawnCrasher(census);
             spawnBosses(census);
             // Bots
-              if (bots.length < c.BOTS) {
-                    let o = new Entity(room.random());
-                    o.color = 90;
+                if (bots.length < c.BOTS) {
+                    let o = new Entity(room.randomType('bas1'));
+                    o.color = 10;
                     o.define(Class.bot);
-                    o.define(Class.elite_gunner);
+                    o.define(Class.basic);
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes();
-                    o.color = 90;
+                    o.team = -1
                     bots.push(o);
                 }
                 // Remove dead ones
